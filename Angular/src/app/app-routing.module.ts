b8552d235af40749a8307c01e0441f920ab97aab
nailaV <naila.vejo@edu.fit.ba>
2022-12-03 16:31:31 +0100

    roterLink

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {LogInComponent} from "./log-in/log-in.component";
+import {StoriesComponent} from "./stories/stories.component";
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'about', component:AboutComponent},
-  {path: 'logIn', component:LogInComponent}
+  {path: 'logIn', component:LogInComponent},
+  {path: 'stories', component: StoriesComponent}
   ]
 
 @NgModule({
